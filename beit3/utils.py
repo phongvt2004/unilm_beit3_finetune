5ef47b66544284694f3b997617673f84c711f171
phonghocode <ntp11892vt@gmail.com>
2025-03-07 02:36:24 +0700

    update training code and checkpoint

--- beit3/utils.py
+++ beit3/utils.py
@@ -24,6 +24,21 @@ import torch.nn.functional as F
 from torch import inf
 from torchmetrics import Metric
 from tensorboardX import SummaryWriter
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+import numpy as np
+
+def compute_metrics(logits, labels):
+    predictions = np.argmax(logits, axis=-1)  # Get class with highest probability
+
+    accuracy = accuracy_score(labels, predictions)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, predictions, average="weighted", zero_division=0)
+
+    return {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1
+    }
 
 
 def bool_flag(s):
