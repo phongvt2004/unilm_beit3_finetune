bf62022c30ae098b61f95e4b421abffcc9c3f265
phonghocode <ntp11892vt@gmail.com>
2025-03-07 22:24:36 +0700

    fix train code

--- beit3/datasets.py
+++ beit3/datasets.py
@@ -418,8 +418,7 @@ class VQADataset(BaseDataset):
         item = self.dataframe.iloc[index]
         answer = item["answer"]
         labels = [0.] * len(self.answer2id)
-        labels[self.answer2id[answer]] = 1
-        data["labels"] = torch.FloatTensor(labels)
+        labels = torch.tensor(self.answer2id[answer], dtype=torch.long)
         return data
 
 class RetrievalDataset(BaseDataset):
