963af0676f27445cf3aec32199472210db85c505
phonghocode <ntp11892vt@gmail.com>
2025-03-10 19:34:33 +0700

    update

--- beit3/datasets.py
+++ beit3/datasets.py
@@ -351,7 +351,7 @@ class ImageNetDataset(BaseDataset):
 
 class VQADataset(BaseDataset):
     def __init__(self, data_path, split, transform, root_folder, tokenizer, num_max_bpe_tokens):
-        self.dataframe = pd.read_csv(os.path.join(data_path, f"{split}.csv"))[:num_sample]
+        self.dataframe = pd.read_csv(os.path.join(data_path, f"{split}.csv"))
         self.dataframe.dropna(inplace=True)
         df = pd.read_csv(os.path.join(data_path, f"data.csv"))
         unique_answers = set(df["answer"].tolist())
